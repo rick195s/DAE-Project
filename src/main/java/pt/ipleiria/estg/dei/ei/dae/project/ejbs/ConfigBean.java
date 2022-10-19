823201c1727c8f961b0a898f44f891e6d1667c1a
Ricardo Franco <ricardosf195@gmail.com>
2022-10-19 16:46:51 +0100

    consume API example

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ejbs/ConfigBean.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ejbs/ConfigBean.java
@@ -4,6 +4,10 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Startup
 @Singleton
@@ -15,7 +19,30 @@ public class ConfigBean {
     public void populateDB() {
         System.out.println("Hello Java EE!");
 
-        courseBean.create(1, "Engenharia Informática");
+        try {
+            URL url = new URL("https://634f1183df22c2af7b4a4b38.mockapi.io/Students");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("Accept", "application/json");
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.connect();
+            System.out.println(con.getResponseCode());
+            System.out.println(con.getResponseMessage());
+
+            BufferedReader responseReader = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String responseLine;
+            while ((responseLine = responseReader.readLine()) != null) {
+                System.out.println(responseLine);
+            }
+
+            responseReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
