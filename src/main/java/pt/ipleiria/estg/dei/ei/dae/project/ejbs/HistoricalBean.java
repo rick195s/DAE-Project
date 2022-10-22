04275c00de90704c6202a180c97283db1e69e4ce
Pedro Braz <pedrobraz2002@gmail.com>
2022-10-22 18:25:45 +0100

    Created entity, bean and DTO: Insurer, Historical, Insurer_Expert

---
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ejbs/HistoricalBean.java
@@ -0,0 +1,28 @@
+package pt.ipleiria.estg.dei.ei.dae.project.ejbs;
+
+import pt.ipleiria.estg.dei.ei.dae.project.enteties.Historical;
+import pt.ipleiria.estg.dei.ei.dae.project.enteties.Historical_Enum;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Date;
+
+@Stateless
+public class HistoricalBean {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void create(int id, String name, String description, Date date) {
+        Historical historical = findHistorical(id);
+        if (historical != null) {
+            throw new IllegalArgumentException("Historical already exists");
+        }
+        historical = new Historical(id, Historical_Enum.A_AGUARDAR_APROVACAO_PELA_SEGURADORA, description, date);
+        entityManager.persist(historical);
+    }
+
+    public Historical findHistorical(int id) {
+        return entityManager.find(Historical.class, id);
+    }
+}
---
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ejbs/InsurerBean.java
@@ -0,0 +1,26 @@
+package pt.ipleiria.estg.dei.ei.dae.project.ejbs;
+
+import pt.ipleiria.estg.dei.ei.dae.project.enteties.Insurer;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class InsurerBean {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void create(int id, String name) {
+        Insurer insurer = findInsurer(id);
+        if (insurer != null) {
+            throw new IllegalArgumentException("Insurer already exists");
+        }
+        insurer = new Insurer(id, name);
+        entityManager.persist(insurer);
+    }
+
+    public Insurer findInsurer(int id) {
+        return entityManager.find(Insurer.class, id);
+    }
+}
