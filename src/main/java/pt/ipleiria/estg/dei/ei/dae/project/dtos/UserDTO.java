cc748e12398531a878cd32b40321aa464c962ea6
Ricardo Franco <ricardosf195@gmail.com>
2022-10-22 16:32:44 +0100

    User related Classes created

---
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/project/dtos/UserDTO.java
@@ -0,0 +1,44 @@
+package pt.ipleiria.estg.dei.ei.dae.project.dtos;
+
+import javax.persistence.Id;
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+    @Id
+    int id;
+
+    String name, email;
+
+    public UserDTO(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public UserDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
