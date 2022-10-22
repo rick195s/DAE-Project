020d6b2bc7072b0c900c0475c0d2127161494918
Jorge Ferreira <48413481+IDontHaveIdeas@users.noreply.github.com>
2022-10-22 18:54:25 +0100

    Add Occurence and Occurence Files 2

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/project/enteties/Occurence.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/project/enteties/Occurence.java
@@ -1,2 +1,104 @@
-package pt.ipleiria.estg.dei.ei.dae.project.enteties;public class Occurence {
+package pt.ipleiria.estg.dei.ei.dae.project.enteties;
+
+import pt.ipleiria.estg.dei.ei.dae.project.enteties.enums.ApprovalType;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@NamedQueries({
+        @NamedQuery(
+                name = "getAllOccurrences",
+                query = "SELECT u FROM Occurence u ORDER BY u.id" // JPQL
+        )
+})
+@Table(
+        name = "occurrences"
+)
+public class Occurence implements Serializable {
+    @Id
+    private int id;
+    private int policyID;
+    private int repairID;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private ApprovalType approvalType;
+    private Date startDate;
+    private Date endDate;
+
+    @OneToMany(fetch=FetchType.LAZY,mappedBy = "occurrence")
+    private List<OccurenceFile> occurenceFileList;
+
+    public Occurence() {
+    }
+
+    public Occurence(int id, int policyID, int repairID, String description, ApprovalType approvalType, Date startDate, Date endDate, List<OccurenceFile> occurenceFileList) {
+        this.id = id;
+        this.policyID = policyID;
+        this.repairID = repairID;
+        this.description = description;
+        this.approvalType = approvalType;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.occurenceFileList = occurenceFileList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPolicyID() {
+        return policyID;
+    }
+
+    public void setPolicyID(int policyID) {
+        this.policyID = policyID;
+    }
+
+    public int getRepairID() {
+        return repairID;
+    }
+
+    public void setRepairID(int repairID) {
+        this.repairID = repairID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ApprovalType getApprovalType() {
+        return approvalType;
+    }
+
+    public void setApprovalType(ApprovalType approvalType) {
+        this.approvalType = approvalType;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
--- src/main/java/pt/ipleiria/estg/dei/ei/dae/project/enteties/OccurenceFile.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/project/enteties/OccurenceFile.java
@@ -1,2 +1,57 @@
-package pt.ipleiria.estg.dei.ei.dae.project.enteties;public class OccurenceFile {
+package pt.ipleiria.estg.dei.ei.dae.project.enteties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+@Entity
+@NamedQueries({
+        @NamedQuery(
+                name = "getAllOccurrencesFiles",
+                query = "SELECT u FROM OccurenceFile u ORDER BY u.id" // JPQL
+        )
+})
+@Table(
+        name = "occurrences_files"
+)
+public class OccurenceFile implements Serializable {
+    @Id
+    private int id;
+    private String name;
+    private String path;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "occurrence_id")
+    private Occurence occurrence;
+
+    public OccurenceFile() {
+    }
+    public OccurenceFile(int id, String name, String path, Occurence occurrence) {
+        this.id = id;
+        this.name = name;
+        this.path = path;
+        this.occurrence = occurrence;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPath() {
+        return this.path;
+    }
+    public void setPath(String path) {
+        this.path = path;
+    }
+    public Occurence getOccurence() {
+        return this.occurrence;
+    }
+    public void setOccurence(Occurence occurrence) {
+        this.occurrence = occurrence;
+    }
 }
