e46d7c050ed743a3b05d5aa7bd0e76fcd4ad6cd5
Miguel Campos <filipe422.11@gmail.com>
2023-01-14 20:18:55 +0000

    get now returns depending on client id or all if user is admin

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ejbs/PolicyBean.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ejbs/PolicyBean.java
@@ -1,17 +1,29 @@
 package pt.ipleiria.estg.dei.ei.dae.project.ejbs;
 
 import pt.ipleiria.estg.dei.ei.dae.project.Supervisor;
+import pt.ipleiria.estg.dei.ei.dae.project.entities.*;
 import pt.ipleiria.estg.dei.ei.dae.project.pojos.Policy;
 import pt.ipleiria.estg.dei.ei.dae.project.pojos.PolicyTypeDetail;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.ArrayList;
 import java.util.List;
 
 @Stateless
 public class PolicyBean {
     @Inject
     Supervisor supervisor;
+    @EJB
+    ClientBean clientBean;
+
+    @PersistenceContext
+    EntityManager entityManager;
 
     public List<Policy> getAllPolicies()
     {
@@ -35,4 +47,41 @@ public class PolicyBean {
     public PolicyTypeDetail getPolicyDetails(int id) {
         return supervisor.getPolicyTypeDetail(id);
     }
+    public Long count() {
+        return supervisor.getPolicies().stream().count();
+    }
+
+    public List<Policy> getAll(User user, int offset, int limit) {
+
+        List<Policy> policies;
+
+        policies = getPolicyOfClient(user);
+        for (Policy policy : policies) {
+        }
+        return policies;
+    }
+
+    private List<Policy> getPolicyOfClient(User user) {
+        List<Policy> policies = new ArrayList<>();
+        switch (user.getRole()) {
+            case CLIENT:
+                Client client = clientBean.find(user.getId());
+                if(client == null) {
+                    return new ArrayList<>();
+                }
+                for (Policy policy : supervisor.getPolicies()) {
+                    if (policy.getClientNIFNIPC() == client.getNIF_NIPC()) {
+                        policies.add(policy);
+                    }
+                }
+                return policies;
+
+            case ADMINISTRATOR:
+                policies = supervisor.getPolicies();
+                 return policies;
+        }
+        return null;
+
+    }
+
 }
--- src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ws/PolicyService.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ws/PolicyService.java
@@ -1,16 +1,27 @@
 package pt.ipleiria.estg.dei.ei.dae.project.ws;
 
+import pt.ipleiria.estg.dei.ei.dae.project.dtos.ErrorDTO;
+import pt.ipleiria.estg.dei.ei.dae.project.dtos.OccurrenceDTO;
+import pt.ipleiria.estg.dei.ei.dae.project.dtos.PaginatedDTO;
 import pt.ipleiria.estg.dei.ei.dae.project.dtos.PolicyDTO;
 import pt.ipleiria.estg.dei.ei.dae.project.dtos.detailed.DetailedPolicyDTO;
 import pt.ipleiria.estg.dei.ei.dae.project.ejbs.PolicyBean;
+import pt.ipleiria.estg.dei.ei.dae.project.ejbs.UserBean;
+import pt.ipleiria.estg.dei.ei.dae.project.entities.Occurrence;
+import pt.ipleiria.estg.dei.ei.dae.project.entities.User;
 import pt.ipleiria.estg.dei.ei.dae.project.pojos.Policy;
 import pt.ipleiria.estg.dei.ei.dae.project.pojos.PolicyTypeDetail;
+import pt.ipleiria.estg.dei.ei.dae.project.requests.PageRequest;
 import pt.ipleiria.estg.dei.ei.dae.project.security.Authenticated;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.EJB;
+import javax.validation.Valid;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import java.util.List;
 
 @Path("policies") // relative url web path for this service
@@ -20,12 +31,41 @@ public class PolicyService {
     @EJB
     private PolicyBean policyBean;
 
+    @EJB
+    private UserBean userBean;
+
+    @Context
+    private SecurityContext securityContext;
+
     @GET
     @Authenticated
     @RolesAllowed({"ADMINISTRATOR", "CLIENT"})
     @Path("/")
-    public List<PolicyDTO> getAllPoliciesWS() {
-        return PolicyDTO.from(policyBean.getAllPolicies());
+    public Response getAllPoliciesWS(@BeanParam @Valid PageRequest pageRequest) {
+        List<Policy> policies;
+
+        Long count = policyBean.count();
+
+        if (pageRequest.getOffset() > count) {
+            return Response.ok(new PaginatedDTO<>(count)).build();
+        }
+
+        User user =  userBean.findUserByEmail(securityContext.getUserPrincipal().getName());
+        policies = policyBean.getAll(user, pageRequest.getOffset(), pageRequest.getLimit());
+        if (policies == null) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity(new ErrorDTO("No occurrences found."))
+                    .build();
+        }
+
+        if (policies.isEmpty()) {
+            count = 0L;
+        }
+
+        var paginatedDTO = new PaginatedDTO<>(PolicyDTO.from(policies), count, pageRequest.getOffset());
+
+        return Response.ok(paginatedDTO).build();
+
     }
 
     @GET
