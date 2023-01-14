d3dbee6ccf8f6326ee47ad9fe94e3e8873053451
Ricardo Franco <ricardosf195@gmail.com>
2023-01-14 18:14:52 +0000

    Update ConfigBean.java

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ejbs/ConfigBean.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ejbs/ConfigBean.java
@@ -68,7 +68,6 @@ public class ConfigBean {
             clientBean.create(faker.name().fullName(), faker.internet().emailAddress(), "123", ((int) faker.number().randomNumber(9, true)));
         }
         clientBean.create("Tiago Santos", "tiago@mail.pt", "123", 333333333);
-    
     }
 
     private void createAdmins() {
